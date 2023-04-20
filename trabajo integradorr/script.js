6461dcb6f983c5ee2f52f351cf66d75732c42b54
MaxiDotta <94641879+MaxiDotta@users.noreply.github.com>
2023-04-20 00:26:11 -0300

    Add files via upload

---
+++ trabajo integradorr/script.js
@@ -0,0 +1,44 @@
+const claveAPI = '9c5c47ee57e83e431155f2989605f396';
+const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+const formulario = $('#formulario');
+const ciudadInput = $('#ciudad');
+
+formulario.validate({
+  rules: {
+    ciudad: {
+      required: true
+    }
+  },
+  messages: {
+    ciudad: {
+      required: 'Por favor ingresa una ciudad'
+    }
+  },
+  submitHandler: function(form) {
+    const ciudad = ciudadInput.val();
+
+    const url = `${urlBase}?q=${ciudad}&appid=${claveAPI}&units=metric`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const temperatura = data.main.temp;
+        const humedad = data.main.humidity;
+        const velocidadViento = data.wind.speed;
+        const temperaturaMaxima = data.main.temp_max;
+        const temperaturaMinina = data.main.temp_min;
+        const codigoIcono = data.weather[0].icon;
+        const urlIcono = `http://openweathermap.org/img/wn/${codigoIcono}.png`
+        console.log(data)
+
+        $('#temperatura').text(`Temperatura: ${temperatura}°C`);
+        $('#humedad').text(`Humedad: ${humedad}%`);
+        $('#velocidad-viento').text(`Velocidad del viento: ${velocidadViento} km/h`);
+        $('#tempearatura_max').text(`máxima: ${temperaturaMaxima}°C`);
+        $('#temperatura_min').text(`minima: ${temperaturaMinina}°C`);
+        $('#icon').attr('src', urlIcono);
+
+
+      });
+  }
+});
